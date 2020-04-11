3ba76883b7eedd4664481c2d6492b137d90429c5
nom <deathdisco@protonmail.com>
2020-04-11 18:13:17 +1000

    command line parser

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,17 @@ mod models;
 pub use error::*;
 pub(crate) use models::*;
 
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "cassette")]
+struct Opt {
+    /// Input file
+    #[structopt(short, long, parse(from_os_str))]
+    source: PathBuf,
+}
+
 fn main() {
     match run() {
         Ok(_) => println!("\n"),
@@ -14,7 +25,9 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let file_content = files::read_file(std::path::PathBuf::from("examples/projects/functions.cassette"))?;
+    let opt = Opt::from_args();
+
+    let file_content = files::read_file(opt.source)?;
     let (_, nodes) = templar::parser::run(&format!("{}\n", file_content)).unwrap();
     let nodes = templar::postprocessor::run(nodes)?;
 
