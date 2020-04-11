78a07cb7b610e3bc002ba844e7aa70affb05ed1b
nom <deathdisco@protonmail.com>
2020-04-11 17:46:42 +1000

    node unwinding

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
 fn run() -> Result<(), Box<dyn std::error::Error>> {
     let file_content = files::read_file(std::path::PathBuf::from("examples/projects/functions.cassette"))?;
     let (_, nodes) = templar::parser::run(&format!("{}\n", file_content)).unwrap();
-    let nodes = templar::preprocessor::run(nodes);
+    let nodes = templar::postprocessor::run(nodes);
 
     // println!("result: {:?}", e);
     // let interpreter     = interpreter::CassetteInterpreter::new();
